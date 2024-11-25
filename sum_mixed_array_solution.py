09e9ea244dd2996b2c53b3656fbc833159729522
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-24 16:00:01 -0800

    create a function that will add all indices of the mixed_array and return the sum.

--- sum_mixed_array_solution.py
+++ sum_mixed_array_solution.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+# Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+# Return your answer as a number.
+
+mixed_array = ['5', '0', 9, 3, 2, 1, '9', 6, 7]
+
+def sum_mix(arr):
+    print(arr)
+    
+sum_mix(mixed_array)
