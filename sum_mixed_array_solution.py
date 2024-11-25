aaf11bbecaf3e2b473612bf0a5fd9b1ac6c808c2
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-24 17:31:23 -0800

    create a successful imbedded while loop that will add and store the sum of the current index in a 'result' variable. print the 'result' when the loop condition is satisfied.

--- sum_mixed_array_solution.py
+++ sum_mixed_array_solution.py
@@ -3,8 +3,17 @@
 # Return your answer as a number.
 
 mixed_array = ['5', '0', 9, 3, 2, 1, '9', 6, 7]
+# mixed_array = ['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']
+# mixed_array = ['1', '5', '8', 8, 9, 9, 2, '3']
 
 def sum_mix(arr):
-    for i in range(len(arr)):
-        print(arr[i])
+    int_array = [int(item) for item in arr]
+    result = 0
+    i = 0
+    while result <= len(int_array):
+        for i in range(len(int_array)):
+            result += int_array[i]
+            print(f"Current index of [i]: {int_array[i]}")
+        print(result)
+        
 sum_mix(mixed_array)
